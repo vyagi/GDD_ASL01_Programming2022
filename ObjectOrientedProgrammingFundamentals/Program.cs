def5786212ab0c0e0738408e38736e46db82b8ac
vyagi <marcin.jagiela@hotmail.com>
2022-11-07 17:20:29 +0100

    Car started

---
+++ ObjectOrientedProgrammingFundamentals/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ObjectOrientedProgrammingFundamentals
+{
+    class Car
+    {
+        private readonly string _brand;
+        private readonly string _make;
+        private readonly int _tankCapacity;
+        private readonly double _fuelConsumption;
+
+        private double _fuelLevel;
+        private double _odometer;
+
+        public Car(string brand, string make, int tankCapacity, double fuelConsumption)
+        {//add necessary checks 
+            _brand = brand;
+            _make = make;
+            _tankCapacity = tankCapacity;
+            _fuelConsumption = fuelConsumption;
+        }
+
+        public string Brand => _brand;
+        public string Make => _make;
+        public int TankCapacity => _tankCapacity;
+        public double FuelConsumption => _fuelConsumption;
+
+        public double FuelLevel => _fuelLevel;
+
+        public int Odometer => (int)_odometer;
+
+        public void AddFuel(int howMuch)
+        { //TODO:(homework) add checks
+            _fuelLevel += howMuch;
+
+            if (_fuelLevel > _tankCapacity)
+                _fuelLevel = _tankCapacity;
+        }
+
+        public void Drive(int howFar)
+        {
+            var maxRange = _fuelLevel / _fuelConsumption * 100;
+
+            if (howFar <= maxRange)
+            {
+                _odometer += howFar;
+                _fuelLevel -= howFar * _fuelConsumption / 100;
+            }
+            else
+            {
+                _odometer += maxRange;
+                _fuelLevel = 0;
+            }
+           
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student s1 = new Student("James Bond", new DateTime(1950, 02, 10));
+            //Console.WriteLine(s1.ToString());
+            //Console.WriteLine(s1.Age);
+            var c = new Car("Opel", "Insignia", 60, 6);
+            c.AddFuel(50);
+            Console.WriteLine(c.FuelLevel);
+            c.Drive(100);
+            Console.WriteLine(c.FuelLevel);
+            Console.WriteLine(c.Odometer);
+            c.Drive(10000);
+            Console.WriteLine(c.FuelLevel);
+            Console.WriteLine(c.Odometer);
+        }
+    }
+}
---
+++ ObjectOrientedProgrammingFundamentals/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ObjectOrientedProgrammingFundamentals
+{
+    class Student
+    {
+        private string _name;
+        private DateTime _dateOfBirth;
+
+        public Student(string fullName, DateTime dateOfBirth)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))
+                throw new ArgumentNullException(nameof(fullName), "Name should be valid");
+                        
+            _name = fullName;
+
+            if (true) //some checks for birthdate - your homework
+            {
+
+            }
+            _dateOfBirth = dateOfBirth;
+        }
+
+        //public int GetAge()  // Don't do it, it's not the Java world
+        //{
+        //    return _age;
+        //}
+
+        //public void SetAge(int newAge)
+        //{
+        //    if (newAge < 1 || newAge > 100)
+        //        throw new ArgumentOutOfRangeException(nameof(newAge), "Age shoud be from 1 to 100");
+
+        //    _age = newAge;
+        //}
+
+        public int Age => DateTime.Now.Year - _dateOfBirth.Year;
+
+        //As a homework, provide the properties for name
+    }
+}
