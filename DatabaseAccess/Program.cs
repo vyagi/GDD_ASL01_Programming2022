ddb361cdcd4acd93470a60065be89790f9681ef2
Marcin Jagieła <marcin.jagiela@hotmail.com>
2022-12-22 09:27:00 +0100

    Examples done

---
+++ DatabaseAccess/Program.cs
@@ -0,0 +1,23 @@
+﻿//ADO.NET - is not much popular these days
+//you need lots of ceremony to do simple things
+using System.Data;
+using System.Data.SqlClient;
+
+var connectionString = "Data Source=localhost,1433;Initial Catalog=AdventureWorks;User ID=sa;Password=password-1234";
+
+var queryString = @"SELECT * FROM SalesLT.Product";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+    using (var command = new SqlCommand(queryString, connection))
+    {
+        command.CommandType = CommandType.Text;
+        var reader = command.ExecuteReader();
+
+        while(reader.Read())
+        {
+            Console.WriteLine(reader.GetString("Name"));
+        }
+    }
+}
---
+++ DatabaseAccessUsingEntityFramework/Program.cs
@@ -0,0 +1,72 @@
+﻿using DatabaseAccessUsingEntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+using (var dbContext = new AdventureWorksContext())
+{
+    //ReadProducts(dbContext);
+    //ReadCustomersWithAddresses(dbContext);
+    //UpdateExample(dbContext);
+    //DeleteExample(dbContext);
+    InsertExample(dbContext);
+}
+
+void InsertExample(AdventureWorksContext dbContext)
+{
+    var newAddress = new Address
+    {
+        AddressLine1 = "Sucharskiego",
+        AddressLine2 = "2",
+        City = "Rzeszów",
+        CountryRegion = "Podkarpackie",
+        PostalCode = "35-225",
+        StateProvince = "Rzeszów - miasto"
+    };
+
+    dbContext.Addresses.Add(newAddress);
+    dbContext.SaveChanges();
+}
+
+static void ReadProducts(AdventureWorksContext dbContext)
+{
+    foreach (var product in dbContext.Products)
+    {
+        Console.WriteLine($"{product.Name} ({product.ProductId}) {product.Color}");
+    }
+}
+
+static void ReadCustomersWithAddresses(AdventureWorksContext dbContext)
+{
+    var customers = dbContext.Customers
+        .Include(x => x.CustomerAddresses)
+        .ThenInclude(x => x.Address)
+        .Where(x => x.CustomerAddresses.Count > 0)
+        .OrderByDescending(x => x.CustomerAddresses.Count);
+
+    foreach (var customer in customers) //!! keep in mind that your work with db under the hood
+    {
+        Console.WriteLine($"{customer.FirstName} {customer.LastName} has {customer.CustomerAddresses.Count} addresses");
+        foreach (var address in customer.CustomerAddresses)
+        {
+            Console.WriteLine($"   {address.Address.City}");
+        }
+    }
+}
+
+static void UpdateExample(AdventureWorksContext dbContext)
+{
+    var firstCustomer = dbContext.Customers.First(x => x.FirstName == "Christopher" && x.LastName == "Beck");
+
+    Console.WriteLine(firstCustomer.Suffix);
+
+    firstCustomer.Suffix = "Sr.";
+    dbContext.SaveChanges();
+}
+
+static void DeleteExample(AdventureWorksContext dbContext)
+{
+    var salesOrderDetailToDelete = dbContext.SalesOrderDetails.First(x => x.SalesOrderDetailId == 110562);
+    Console.WriteLine(salesOrderDetailToDelete.LineTotal);
+
+    dbContext.SalesOrderDetails.Remove(salesOrderDetailToDelete);
+    dbContext.SaveChanges();
+}
